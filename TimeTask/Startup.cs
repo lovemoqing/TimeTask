3bb915be430ba39a3426bc3efef3d70099e76206
penglong <penglong@tidebuy.net>
2018-11-15 19:20:47 +0800

    定时发送短信

--- TimeTask/Startup.cs
+++ TimeTask/Startup.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Profile;
+using Aliyun.Acs.Dysmsapi.Model.V20170525;
+using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
 namespace TimeTask
 {
+    /// <summary>
+    /// 定时发送短信
+    /// Remarks:需要自配数据库链接字符串，以及短信API
+    /// </summary>
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -22,11 +30,17 @@ namespace TimeTask
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddHangfire(x => x.UseSqlServerStorage(""));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -45,6 +59,69 @@ namespace TimeTask
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.Map("/index", r =>
+            {
+                r.Run(context =>
+                {
+                    //任务每分钟执行一次
+                    RecurringJob.AddOrUpdate(() => Console.WriteLine($"ASP.NET Core LineZero"), Cron.Minutely());
+                    return context.Response.WriteAsync("ok");
+                });
+            });
+
+            app.Map("/one", r =>
+            {
+                r.Run(context =>
+                {
+                    //任务执行一次
+                    BackgroundJob.Enqueue(() => SendSms());
+                    return context.Response.WriteAsync("ok");
+                });
+            });
+            app.Map("/await", r =>
+            {
+                r.Run(context =>
+                {
+                    //任务延时两分钟执行
+                    BackgroundJob.Schedule(() => Console.WriteLine($"ASP.NET Core await LineZero{DateTime.Now}"), TimeSpan.FromMinutes(2));
+                    return context.Response.WriteAsync("ok");
+                });
+            });
+        }
+        public void SendSms()
+        {
+            String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
+            String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
+            String accessKeyId = "";//你的accessKeyId，参考本文档步骤2
+            String accessKeySecret = "";//你的accessKeySecret，参考本文档步骤2
+            IClientProfile profile = DefaultProfile.GetProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+            //IAcsClient client = new DefaultAcsClient(profile);
+            // SingleSendSmsRequest request = new SingleSendSmsRequest();
+            //初始化ascClient,暂时不支持多region（请勿修改）
+            DefaultProfile.AddEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+            IAcsClient acsClient = new DefaultAcsClient(profile);
+            SendSmsRequest request = new SendSmsRequest();
+            try
+            {
+                //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式，发送国际/港澳台消息时，接收号码格式为00+国际区号+号码，如“0085200000000”
+                request.PhoneNumbers = "";
+                //必填:短信签名-可在短信控制台中找到
+                request.SignName = "";
+                //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
+                request.TemplateCode = "";
+                //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+                request.TemplateParam = "{\"name\":\"彭彭\"}";
+                //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+                request.OutId = "yourOutId";
+                //请求失败这里会抛ClientException异常
+                SendSmsResponse sendSmsResponse = acsClient.GetAcsResponse(request);
+                System.Console.WriteLine(sendSmsResponse.Message);
+            }
+            catch (Exception)   
+            {
+                System.Console.WriteLine("Hello World!");
+            }
         }
     }
 }
